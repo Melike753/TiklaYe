f90b2299672caaa6ba275fb2a778747bdd01fcd9
Melike753 <dnmzmelikeee@gmail.com>
2024-07-11 06:35:29 +0300

    update

---
+++ TiklaYe/Controllers/FeedbackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TiklaYe.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
